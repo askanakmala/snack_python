677270a9410e9e533229264d9cd64c967244a065
Askan28 <46845990+askanakmala@users.noreply.github.com>
2020-12-25 20:42:54 +0700

    Typo

--- windows-wifi-pass-extract.py
+++ windows-wifi-pass-extract.py
@@ -1,10 +1,10 @@
 import subprocess
 
-data = subprocess.check_output(['nesth', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 wifis = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]
 
 for wifi in wifis:
-    hasil = subprocess.check_output(['nesth', 'wlan', 'show', 'profiles', wifi, 'key=clear']).decode('utf-8').split('\n')
+    hasil = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', wifi, 'key=clear']).decode('utf-8').split('\n')
     hasil = [line.split(':')[1][1:-1] for line in hasil if "Key Content" in line]
     try:
         print(f'Name: {wifi}, Password: {hasil[0]}')
